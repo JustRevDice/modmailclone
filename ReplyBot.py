5878fc8398a8eb9996f30d7e559c90185d0d4fcd
JustRevDice <93582017+JustRevDice@users.noreply.github.com>
2022-07-11 03:29:11 +0200

    Update ReplyBot.py

--- ReplyBot.py
+++ ReplyBot.py
@@ -2,11 +2,11 @@ import praw
 import random
 import time
 reddit = praw.Reddit(
-    client_id="UaK4A3wPEiJ3bpRWptKGZg",
-    client_secret="VmOw0mDU300-F0iOWQWgbkrNwnjETA",
+    client_id=" ",
+    client_secret=" ",
     user_agent="27 Bibles bot, v0.1 by u/BasedOnDeezNuts and u/JustRevDice",
-    username="DevourerOf27Bibles01",
-    password="BrasilKindaBased1",
+    username=" ",
+    password=" ",
 )
 submissions = reddit.redditor("JustRevDice").new()
 pog = list(submissions)
